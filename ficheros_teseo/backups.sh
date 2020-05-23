80b785a68a30b098fe85e16ddf7659e5830fbffe
manueljesus00 <mjfloresmontano@gmail.com>
2020-05-24 00:05:06 +0200

    Modify backup script and upload exploration file

--- ficheros_teseo/backups.sh
+++ ficheros_teseo/backups.sh
@@ -4,6 +4,7 @@
 ## Nombre del fichero: backups.SH
 ## Ruta recomendada: /home/scripts/backups.SH
 ## Fecha creación: 13-Mayo-2020
+## Fecha modificación: 17-Mayo-2020
 ## Equipo: TESEO
 ## Autor: Manuel Jesus Flores Montaño
 ## Github: @manueljesus00
@@ -15,3 +16,4 @@ fecha=`date +%y-%m-%d-%H:%M`
 # Ejecutar la copia de seguridad
 # Solo copia los directorios personales
 rsync -aav -e 'ssh -i /ssh_keys/atenea' -b --backup-dir = manuel@atenea.pasir1920.local:/home /backups/atenea/$fecha >> /backups/logs/atenea/$fecha.log
+rsync -aav -e 'ssh -i /ssh_keys/heracles' -b --backup-dir = manuel@heracles.pasir1920.local:/home /backups/heracles/$fecha >> /backups/logs/heracles/$fecha.log
